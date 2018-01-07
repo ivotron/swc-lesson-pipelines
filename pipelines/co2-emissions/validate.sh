a71ce9a7444f349c84c43aca8dd9aa12c2d2e3b7
Ivo Jimenez <ivo.jimenez@gmail.com>
2018-01-06 22:55:45 -0600

    Updates comments to remove colon

--- pipelines/co2-emissions/validate.sh
+++ pipelines/co2-emissions/validate.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# [wf]: validate results and get a table
+# [wf] validate results and get a table
 set -ex
 
-# [wf]: verify that we got actual result values
+# [wf] verify that we got actual result values
 scripts/validate_output.py data/global_per_capita_mean.csv
 
-# [wf]: generate markdown table
+# [wf] generate markdown table
 scripts/get_mdown_table.py data/global_per_capita_mean.csv
